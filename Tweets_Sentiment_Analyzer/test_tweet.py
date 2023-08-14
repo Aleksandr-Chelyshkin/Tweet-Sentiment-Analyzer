f3e036a931a54483f28f529e67e0c82566048ddd
Aleksandr <137812183+Aleksandr-Chelyshkin@users.noreply.github.com>
2023-08-14 07:44:11 +0200

    Update test_tweet.py

--- Tweets_Sentiment_Analyzer/test_tweet.py
+++ Tweets_Sentiment_Analyzer/test_tweet.py
@@ -1,11 +1,14 @@
+import argparse
 import unittest
 from Tweets_Analyzer import TwitterSentimentAnalyzer as BaseTwitterSentimentAnalyzer
 import tweepy
 
-api_key = ".."
-api_secret = ".."
-access_token = ".."
-access_token_secret = ".."
+parser = argparse.ArgumentParser()
+parser.add_argument('--key', help='Your Twitter API key')
+parser.add_argument('--ksecret', help='Your Twitter API secret key')
+parser.add_argument('--token', help='Your Twitter access token')
+parser.add_argument('--tsecret', help='Your Twitter access token secret')
+args = parser.parse_args()
 
 class TwitterSentimentAnalyzer(BaseTwitterSentimentAnalyzer):
     def __init__(self, api_key, api_secret, access_token, access_token_secret):
@@ -13,9 +16,10 @@ class TwitterSentimentAnalyzer(BaseTwitterSentimentAnalyzer):
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
 
+
 class TestTwitterSentimentAnalyzer(unittest.TestCase):
     def test_search_tweets(self):
-        analyzer = TwitterSentimentAnalyzer(api_key, api_secret, access_token, access_token_secret)
+        analyzer = TwitterSentimentAnalyzer(args.key, args.ksecret, args.token, args.tsecret)
         results = analyzer.analyze("test query")
         assert isinstance(results, dict)
         assert "positive" in results
