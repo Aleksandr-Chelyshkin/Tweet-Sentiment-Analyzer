33497930c5fbcd3b1b4a58cd719d95879a5eeacc
Aleksandr <137812183+Aleksandr-Chelyshkin@users.noreply.github.com>
2023-08-15 16:25:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="Tweets_Analyzer",
-    version="0.6",
+    version="0.1",
     description="A Python package for analyzing the sentiment of tweets.",
     author="Aleksandr",
     author_email="novacyan545@gmail.com",
     url="https://github.com/Aleksandr-Chelyshkin/Tweets_Sentiment_Analyzer.git",
     packages=find_packages(),
-    install_requires=["tweepy", "textblob", "python-dotenv"],
+    install_requires=["tweepy", "textblob"],
 )
